a070698d794b3c165dc407b54340429992bb0ffd
surya20000 <141112445+surya20000@users.noreply.github.com>
2023-11-25 18:11:23 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Editor from './Editor';
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className='input'>
+            <Editor/>
+      </div>
+      <div>
+       
+      </div>
+    </div>
+  );
+}
+
+export default App;
